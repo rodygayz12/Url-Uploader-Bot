1aa4801f0f2e34ce9c98fcb86654e915e4d91d44
rodygayz12 <161971854+rodygayz12@users.noreply.github.com>
2024-04-02 00:41:00 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,24 @@ const botgram = require("botgram")
 const bot = botgram("6861294909:AAEf5aupZPVgBeWmfM864AWcLSoRyutDrnY")
 
 bot.command("start", "help", (msg, reply) =>
-  reply.text("To schedule an alert, do: /alert <seconds> <text>"))
+  reply.text("Wellcome to Url Uploader Bot"))
 
-bot.command("alert", (msg, reply, next) => {
-  var [ seconds, text ] = msg.args(2)
-  if (!seconds.match(/^\d+$/) || !text) return next()
+});
 
-  setTimeout(() => reply.text(text), Number(seconds) * 1000)
-})
+bot.text((msg, reply) => {
+    const url = msg.text;
 
-bot.command((msg, reply) =>
-  reply.text("Invalid command."))
+    fetch(url)
+        .then(response => {
+            const fileStream = fs.createWriteStream('downloaded_file');
+            response.body.pipe(fileStream);
+
+            fileStream.on('finish', () => {
+                reply.document('downloaded_file');
+            });
+        })
+        .catch(err => {
+            console.error('Error downloading file:', err);
+            reply.text('Error downloading file.');
+        });
+});
